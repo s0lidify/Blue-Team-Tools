9a3ba00555c67fc660602958c9be07c422f91eeb
cyberspectro <149823813+spectrumlight@users.noreply.github.com>
2025-03-03 20:29:24 +0200

    Update integrity.py

--- Integrity/integrity.py
+++ Integrity/integrity.py
@@ -3,13 +3,13 @@ import os
 import sys
 import time
 
-def calculate_sha256(file_path):
-    sha256 = hashlib.sha256()
+def calculate_sha1(file_path):
+    sha1 = hashlib.sha1()
     try:
         with open(file_path, 'rb') as f:
             while chunk := f.read(4096):
-                sha256.update(chunk)
-        return sha256.hexdigest()
+                sha1.update(chunk)
+        return sha1.hexdigest()
     except FileNotFoundError:
         print("Error: File not found.")
         sys.exit(1)
@@ -27,9 +27,9 @@ def main():
     os.makedirs(save_dir, exist_ok=True)
     
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-    sha256_hash = calculate_sha256(file_path)
+    sha1_hash = calculate_sha1(file_path)
     
-    result = f"[{timestamp}] - [{file_path}] - [sha256:{sha256_hash}]\n"
+    result = f"[{timestamp}] - [{file_path}] - [sha1:{sha1_hash}]\n"
     print(result.strip())
     
     result_file = os.path.join(save_dir, "integrity.txt")
