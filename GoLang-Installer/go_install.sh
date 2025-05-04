f57afd147c5625c63c62cbfaba97dfc65031ff8d
cyberspectro <149823813+spectrumlight@users.noreply.github.com>
2025-05-04 23:21:26 +0300

    Update go_install.sh

--- GoLang-Installer/go_install.sh
+++ GoLang-Installer/go_install.sh
@@ -14,10 +14,10 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 mkdir ~/go_installer
-wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
 cd ~/go_installer
+wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.24.2.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" > /etc/profile
+echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 source /etc/profile
 
 echo -e "${BLUE}Done! Sometimes you need to refresh the shell${NC}"
