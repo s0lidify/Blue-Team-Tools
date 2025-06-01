26259fad79de47d5f53e2121873f3bc7a4975455
cyberspectro <149823813+spectrumlight@users.noreply.github.com>
2025-06-02 00:02:44 +0300

    Update xrdp_ubuntu_installer.sh

--- XRDP-Ubuntu-Installer/xrdp_ubuntu_installer.sh
+++ XRDP-Ubuntu-Installer/xrdp_ubuntu_installer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 sudo apt update && sudo apt install -y xrdp xfce4 xfce4-goodies
 echo "startxfce4" | sudo tee /etc/xrdp/startwm.sh
 sudo chmod +x /etc/xrdp/startwm.sh
@@ -11,4 +9,4 @@ sudo chown xrdp:xrdp /home/$USER/.Xauthority
 sudo adduser xrdp ssl-cert
 sudo systemctl restart xrdp
 echo "exec /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &" >> ~/.xsession
-sudo reboot
+#sudo reboot
